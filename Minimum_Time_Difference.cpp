9c29105d063b5b6a7d25ff16ee87a35240db89fd
Pochana Srikar Reddy <59635392+SRIKARREDDY-dotorg@users.noreply.github.com>
2024-09-16 09:15:10 +0530

    Update Minimum_Time_Difference.cpp

--- Minimum_Time_Difference.cpp
+++ Minimum_Time_Difference.cpp
@@ -42,3 +42,29 @@ public:
         return minTime;
     }
 };
+
+
+// Other way
+class Solution {
+public:
+    int findMinDifference(vector<string>& timePoints) {
+        int n = timePoints.size();
+        vector<int> mins(n);
+
+        for(int i = 0; i < timePoints.size(); i ++) {
+            string time = timePoints[i];
+            int h = stoi(time.substr(0, 2));
+            int m = stoi(time.substr(3));
+            mins[i] = h*60 + m;
+        }
+
+        sort(mins.begin(), mins.end());
+
+        int ans = INT_MAX;
+        for(int i = 0; i < mins.size() - 1; i ++) {
+            ans = min(ans, mins[i+1] - mins[i]);
+        }
+
+        return min(ans, 24*60 - mins[n-1] + mins[0]);
+    }
+};
