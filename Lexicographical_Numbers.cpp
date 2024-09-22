329bcd97bf11fce94563fc457ef2512fa0ed86ab
Pochana Srikar Reddy <59635392+SRIKARREDDY-dotorg@users.noreply.github.com>
2024-09-22 09:03:40 +0530

    Update Lexicographical_Numbers.cpp

--- Lexicographical_Numbers.cpp
+++ Lexicographical_Numbers.cpp
@@ -14,3 +14,26 @@ public:
         return result;
     }
 };
+
+
+// other approach
+class Solution {
+public:
+    vector<int> lexicalOrder(int n) {
+        vector<int> lexicalOrderNumbers;
+        int currentNumber = 1;
+        for(int i = 0; i < n; i++) {
+            lexicalOrderNumbers.push_back(currentNumber);
+
+            if(currentNumber*10 <= n) {
+                currentNumber*=10;
+            } else {
+                while(currentNumber%10 == 9 || currentNumber >=n) {
+                    currentNumber/=10;
+                }
+                currentNumber+=1;
+            }
+        }
+        return lexicalOrderNumbers;
+    }
+};
