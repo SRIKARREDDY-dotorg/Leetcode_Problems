a0a2e7438e114e976cd8a23424df5056d7f8794c
Pochana Srikar Reddy <59635392+SRIKARREDDY-dotorg@users.noreply.github.com>
2025-04-29 10:43:38 +0530

    Update Wildcard_Matching.cpp

--- Wildcard_Matching.cpp
+++ Wildcard_Matching.cpp
@@ -22,3 +22,29 @@ public:
         return fun(s, p, 0, 0, s.size(), p.size());
     }
 };
+
+// Iterative
+class Solution {
+public:
+    bool isMatch(string s, string p) {
+        int s_size = s.size();
+        int p_size = p.size();
+        vector<vector<bool>> dp(s_size+1, vector<bool>(p_size+1, 0));
+        dp[0][0] = 1;
+        for(int j = 1; j <= p_size; j ++) {
+            if(p[j-1] == '*') {
+                dp[0][j] = dp[0][j-1];
+            }
+        }
+        for(int i = 1; i <= s_size; i++) {
+            for(int j = 1; j <= p_size; j++) {
+                if(s[i-1] == p[j-1] || p[j-1] == '?') {
+                    dp[i][j] = dp[i-1][j-1];
+                } else if(p[j-1] == '*') {
+                    dp[i][j] = dp[i][j-1] || dp[i-1][j];
+                }
+            }
+        }
+        return dp[s_size][p_size];
+    }
+};
