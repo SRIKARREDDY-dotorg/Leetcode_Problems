db46bf12304d673c4df3fb046b8b20836a521333
Pochana Srikar Reddy <59635392+SRIKARREDDY-dotorg@users.noreply.github.com>
2025-06-30 08:26:13 +0530

    Update Longest_Harmonious_Subsequence.cpp

--- Longest_Harmonious_Subsequence.cpp
+++ Longest_Harmonious_Subsequence.cpp
@@ -1,4 +1,4 @@
-https://leetcode.com/problems/longest-harmonious-subsequence/?envType=daily-question&envId=2025-06-30
+// https://leetcode.com/problems/longest-harmonious-subsequence/?envType=daily-question&envId=2025-06-30
 
 class Solution {
 public:
@@ -15,6 +15,25 @@ public:
             if(abs(nums[i]-nums[j]) == 1)
                 maxLen = max(maxLen, i-j+1);
             i++;
+        }
+        return maxLen;
+    }
+};
+
+// Further optimised
+
+class Solution {
+public:
+    int findLHS(vector<int>& nums) {
+        unordered_map<int, int> hashMap;
+        for(auto& num: nums) {
+            hashMap[num]++;
+        }
+        int maxLen = 0;
+        for(auto& [num, freq]: hashMap) {
+            if(hashMap.count(num+1)) {
+               maxLen = max(maxLen, freq+hashMap[num+1]);
+            }
         }
         return maxLen;
     }
