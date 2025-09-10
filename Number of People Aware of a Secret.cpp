8498c5baeaff7fa08b8ccb0b47e502293418d8ff
Pochana Srikar Reddy <59635392+SRIKARREDDY-dotorg@users.noreply.github.com>
2025-09-10 11:28:03 +0530

    Create Number of People Aware of a Secret.cpp

---
+++ Number of People Aware of a Secret.cpp
@@ -0,0 +1,34 @@
+// https://leetcode.com/problems/number-of-people-aware-of-a-secret/?envType=daily-question&envId=2025-09-09
+class Solution {
+public:
+    int peopleAwareOfSecret(int n, int delay, int forget) {
+        deque<pair<int, int>> know, share;
+        int know_cnt = 1, share_cnt = 0;
+        know.emplace_back(1,1);
+
+        for(int i = 2; i <= n; i ++) {
+            if(!know.empty() && know[0].first == i-delay) {
+                know_cnt = (know_cnt - know[0].second + mod)%mod;
+                share_cnt = (share_cnt + know[0].second)%mod;
+                share.emplace_back(know[0]);
+                know.pop_front();
+            }
+
+            if(!share.empty() && share[0].first == i-forget) {
+                share_cnt = (share_cnt - share[0].second + mod)%mod;
+                share.pop_front();
+            }
+            // new people of share count learns from same share_count hence put it in back.
+            // Since these are new people only eligible for the know phase hence add to the know count.
+            if(!share.empty()) {
+                know_cnt = (know_cnt + share_cnt) % mod;
+                know.emplace_back(i, share_cnt);
+            }
+        }
+
+        return (know_cnt + share_cnt) % mod;
+    }
+
+private:
+    static constexpr int mod = 1000000007;
+};
